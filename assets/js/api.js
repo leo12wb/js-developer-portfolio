8655859eb5da0c9a73f7924ee4b81fd15327d9ad
leonardoRang <leonardo@mtw.com.br>
2023-06-09 09:48:44 -0300

    finalizar

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/leo12wb/js-developer-portfolio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
